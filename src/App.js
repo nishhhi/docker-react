39d04f45b00a0185dd865cf658e092324aa0c7d4
Nishita Shetty <Nishita.Shetty@alticeUSA.com>
2021-06-14 17:20:31 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnt React
+          Learn React
         </a>
       </header>
     </div>
